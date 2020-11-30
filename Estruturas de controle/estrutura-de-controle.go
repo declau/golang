6e1453d8893a4e8aa3ed4aee7fef7504f4900fb0
declau <dec82@hotmail.com>
2020-11-30 13:16:14 -0300

    control structures

---
+++ Estruturas de controle/estrutura-de-controle.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Estruturas de Controle")
+
+	//number := 10
+	number := -5
+	//number := -11
+
+	if number > 15 {
+		fmt.Println("Maior que 15")
+	} else {
+		fmt.Println("Menor ou igual a 15")
+	}
+
+	//if init
+	//Ao criar um variável pelo if init ela fica visível somente dentro do escopo do if
+	if anotherNumber := number; anotherNumber > 0 {
+		fmt.Println("Número é maior que 0")
+	} else if anotherNumber < -10 {
+		fmt.Println("Número é menor que -10")
+	} else {
+		fmt.Println("Número esta entre 0 e -10")
+	}
+}
